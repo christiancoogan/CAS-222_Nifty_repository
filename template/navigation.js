397e763834adec7d51855c52bb5ea615455a2647
christiancoogan <christian.coogan@gmail.com>
2017-05-10 16:44:25 -0700

    Add files via upload

---
+++ template/navigation.js
@@ -0,0 +1,21 @@
+function responsiveNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
+}
